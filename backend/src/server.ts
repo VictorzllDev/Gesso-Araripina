d0d71f1cfe5bd33ef8c113f0c848fc833a03dc2c
VictorzllDev <joaovictor12200capa@gmail.com>
2024-03-04 14:51:47 -0300

    backend: settings fastify

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,11 @@
-console.log('Hello world!')
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.listen({ port: 3333 }, (err, address) => {
+	if (err) {
+		console.error(err)
+		process.exit(1)
+	}
+	console.log(`Run server ${address}`)
+})
